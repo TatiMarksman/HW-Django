b700b2003ad6cd2377641ca3d97b01cfd864d875
Tati Marksman <sdushor95@mail.ru>
2024-08-21 07:45:40 +0300

    finished task

--- 3.1-drf-intro/smart_home/smart_home/urls.py
+++ 3.1-drf-intro/smart_home/smart_home/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('measurement.urls')),  # Подключаем маршруты из measurement
+    path('api/', include('measurement.urls')), 
 ]
