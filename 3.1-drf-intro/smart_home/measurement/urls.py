00f1041db2932190a778a2a1fc1f1e2595cf54b0
Tati Marksman <sdushor95@mail.ru>
2024-08-21 07:38:16 +0300

    smart home finished

--- 3.1-drf-intro/smart_home/measurement/urls.py
+++ 3.1-drf-intro/smart_home/measurement/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from .views import SensorListCreateView, SensorRetrieveUpdateView, SensorDetailView, MeasurementListCreateView
 
 urlpatterns = [
-    # TODO: зарегистрируйте необходимые маршруты
+    path('sensors/', SensorListCreateView.as_view(), name='sensor-list-create'),
+    path('sensors/<pk>/', SensorRetrieveUpdateView.as_view(), name='sensor-retrieve-update'),
+    path('sensors/<pk>/detail/', SensorDetailView.as_view(), name='sensor-detail'),
+    path('measurements/', MeasurementListCreateView.as_view(), name='measurement-list-create'),
 ]
